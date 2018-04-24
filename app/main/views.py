29641181ae8ac2e2de670f9cf50032560683f7e3
Mbugua Munga <munga2mbugua@gmail.com>
2018-04-24 15:40:23 +0300

    added view function for different information

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,20 @@
 from . import main
-from flask import render_template
+from flask import render_template,jsonify,request
+import json
+
+# open the json files 
+Activities = open("app/data/parsed_activity.json","r")
+areas = open("app/data/parsed_areas.json","r")
+internet = open ("app/data/parsed_internet.json","r")
+materials = open ("app/data/parsed_materials.json","r")
+population = open ("app/data/parsed_population.json","r")
+
+# load the information into list 
+loaded_activities = json.load(Activities)
+loaded_areas = json.load(areas)
+loaded_internet = json.load(internet)
+loaded_materials = json.load(materials)
+loaded_population = json.load(population)
 
 
 @main.route('/')
@@ -15,3 +30,23 @@ def documentaion():
 @main.route('/source')
 def source():
     return render_template('source.html')
+
+@main.route('/activities',methods = ['Get'])
+def test_activities():
+    return jsonify({"activities" : loaded_activities})
+
+@main.route('/areas',methods = ['Get'])
+def test_areas():
+    return jsonify({"activities" : loaded_areas})
+
+@main.route('/internet',methods = ['Get'])
+def test_internet():
+    return jsonify({"activities" : loaded_internet})
+
+@main.route('/materials',methods = ['Get'])
+def test_materials():
+    return jsonify({"activities" : loaded_materials})
+
+@main.route('/population',methods = ['Get'])
+def test_population():
+    return jsonify({"activities" : loaded_population})
