6f376657a2a3ff273d50f3d8d7968a6f9bfa22a5
sami-mai <samirah.maison@gmail>
2018-04-24 10:23:34 +0300

    update view routes

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,13 @@ from flask import render_template
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/docs')
+def documentaion():
+    return render_template('docs.html')
+
+
+@main.route('/source')
+def source():
+    return render_template('source.html')
