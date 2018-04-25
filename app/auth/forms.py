3d8cae7c1e6fe1498238e85eb229e41eb073b541
sami-mai <samirah.maison@gmail>
2018-04-25 10:49:55 +0300

    add recaptcha field

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
+from flask_wtf.recaptcha import RecaptchaField
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required, Email, EqualTo
-from ..models import User
+# from ..models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -9,11 +10,14 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email ', validators=[Required(), Email()])
     password = PasswordField('Password', validators=[Required(), EqualTo('password_confirm', message='Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords', validators=[Required()])
-    submit = SubmitField('Sign Up')
+    recaptcha = RecaptchaField()
+    terms = BooleanField('I agree to the terms')
+    attribution = BooleanField('I promise to add an attribution link on my website or app to kensusapi.herokuapp.com.')
+    submit = SubmitField('Submit')
 
 
 class LoginForm(FlaskForm):
     email = StringField('Your Email Address', validators=[Required(), Email()])
     password = PasswordField('Password', validators=[Required()])
     remember = BooleanField('Remember me')
-    submit = SubmitField('Sign In')
+    submit = SubmitField('Submit')
