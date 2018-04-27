43f5bd35887752616c4d08a42d3a59ea69450646
sami-mai <samirah.maison@gmail>
2018-04-27 09:15:22 +0300

    successful merge

--- app/main/views.py
+++ app/main/views.py
@@ -2,11 +2,11 @@ from . import main
 from flask import render_template, jsonify, request
 import json
 
-parsed_activities = open('app/data/parsed_activity.json','r')
-parsed_population = open('app/data/parsed_population.json','r')
-parsed_areas = open('app/data/parsed_areas.json','r')
-parsed_materials = open('app/data/parsed_materials.json','r')
-parsed_internet = open('app/data/parsed_internet.json','r')
+parsed_activities = open('app/data/parsed_activity.json', 'r')
+parsed_population = open('app/data/parsed_population.json', 'r')
+parsed_areas = open('app/data/parsed_areas.json', 'r')
+parsed_materials = open('app/data/parsed_materials.json', 'r')
+parsed_internet = open('app/data/parsed_internet.json', 'r')
 
 loaded_activities = json.load(parsed_activities)
 loaded_population = json.load(parsed_population)
@@ -26,6 +26,7 @@ def documentation():
     title = "Documentation"
     return render_template('docs/docs.html', title=title)
 
+
 @main.route('/auth')
 def Authentication():
     title = "Authentication"
@@ -38,7 +39,6 @@ def clientlib():
     return render_template('docs/clientlib.html', title=title)
 
 
-
 @main.route('/endpoints')
 def endpoints():
     title = "Endpoints"
@@ -48,6 +48,7 @@ def endpoints():
 @main.route('/pricing')
 def pricing():
     return render_template('pricing.html')
+
 
 @main.route('/errors')
 def errors():
@@ -60,28 +61,25 @@ def source():
     title = "Sources"
     return render_template('source.html', title=title)
 
+
 @main.route('/terms')
 def terms():
     return render_template('terms.html')
 
+
 # api endpoints
-
-
-@main.route('/Everything',methods = ['GET'])
+@main.route('/Everything', methods=['GET'])
 def test_activities():
-    everything = [{"activities" :loaded_activities},{'population': loaded_population},{'areas': loaded_areas},{'internet':loaded_internet},{'materials':loaded_internet}]
-    return jsonify({'all':everything})
+    everything = [{"activities": loaded_activities}, {'population': loaded_population}, {'areas': loaded_areas}, {'internet': loaded_internet}, {'materials': loaded_internet}]
+    return jsonify({'all': everything})
 
 
-@main.route('/Everthing/<name>',methods = ['Get'])
+@main.route('/Everthing/<name>', methods=['Get'])
 def test_population(name):
-    everything = [{'key':"activities", 'info':loaded_activities},{'key':'population','info':loaded_population},{'key':'areas','info': loaded_areas},{'key':'internet','info' : loaded_internet},{'key':'materials','info':loaded_internet}]
-    
+    everything = [{'key': "activities", 'info': loaded_activities}, {'key': 'population', 'info': loaded_population}, {'key': 'areas', 'info': loaded_areas}, {'key': 'internet', 'info': loaded_internet}, {'key': 'materials', 'info': loaded_internet}]
+
     for detail in everything:
             if detail.get('key') == name:
-                return jsonify(detail) 
+                return jsonify(detail)
 
-        
-
-
-    return jsonify({'message' : 'bad request'})
+    return jsonify({'message': 'bad request'})
